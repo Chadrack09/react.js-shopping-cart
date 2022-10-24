0c77d768578b3fd2db44dfee7c93fe3e73bc1bf1
Chaadrack B <bbenolych@gmail.com>
2022-10-24 17:35:47 +0200

    updated add to cart functionality

--- src/redux/reducers/CartReducer.js
+++ src/redux/reducers/CartReducer.js
@@ -14,16 +14,8 @@ export const addToCartReducer = (state = initialState, action) => {
 
       let product = action.payload;
       let attrSelected = product.attributes.map((attr) =>
-        attr.items.find((item) => item.checked === true))
+        attr.items.find((item) => item.checked === true));
 
-      // let inCart = false;
-      
-      // state.cartItems.forEach((cartItem) => {
-      //   // if (cartItem.id === product.id) {
-      //   if (cartItem.id === product.id) {
-      //     inCart = true;
-      //   }
-      // });
 
       if(state.cartItems.some(item => item.id === product.id &&
         item.attrSelected.every((attr, i) => attr.value === attrSelected[i].value))) {
@@ -38,7 +30,8 @@ export const addToCartReducer = (state = initialState, action) => {
           ),
           totalQty: state.totalQty + 1,
         };
-      } else {
+      } 
+      else {
         return {
           ...state,
           cartItems: [
@@ -52,73 +45,6 @@ export const addToCartReducer = (state = initialState, action) => {
           totalQty: state.totalQty + 1,
         };
       }
-      
-      // return {
-      //   ...state,
-      //   cartItems: inCart // is true
-      //     ? state.cartItems.map((cartItem) =>
-      //         // check if attrSelected is equal to cartAttrSelected
-      //         // attrSelected.length === cartAttrSelected.length &&
-      //         attrSelected.every((attr, i) => attr.id === cartAttrSelected[i].id)
-      //           ? { ...cartItem, qty: cartItem.qty + 1 }
-      //           : // 
-      //           // : [...state.cartItems, { ...product, qty: 1 }]
-      //       )
-          
-      //     : [
-      //       ...state.cartItems, 
-      //       { 
-      //         ...product, 
-      //         attrSelected: product.attributes.map((attr) => 
-      //           attr.items.find((itm) => itm.checked === true)),
-      //         qty: 1,
-      //     }], // inCart is false
-
-      //   totalQty: state.cartItems.reduce((acc, item) => acc + item.qty, 1),
-
-      // return { // good one
-      //   ...state,
-      //   cartItems: inCart // is false
-      //     ? state.cartItems.map((cartItem) =>
-      //         cartItem.id === product.id
-      //           ? {
-      //               ...cartItem,
-      //               attrSelected: // compare cartItem.attrSelected with attrSelected
-      //                 cartItem.attrSelected === attrSelected
-      //                   ? cartItem.attrSelected
-      //                   : attrSelected,
-                        
-      //               qty: cartItem.qty + 1,
-      //             }
-      //           : cartItem
-      //       )
-      //     : [
-      //       ...state.cartItems, 
-      //       { 
-      //         ...product, 
-      //         attrSelected: product.attributes.map((attr) => 
-      //           attr.items.find((itm) => itm.checked === true)),
-      //         qty: 1,
-      //     }], // inCart is true
-
-      //   totalQty: state.cartItems.reduce((acc, item) => acc + item.qty, 1),
-      // };
-      
-      // return {
-      //   ...state,
-      //   cartItems: inCart
-      //     ? state.cartItems.map((cartItem) =>
-      //         cartItem.id === item.id
-      //           ? {
-      //               ...cartItem,
-      //               qty: cartItem.qty + 1,
-      //             }
-      //           : cartItem
-      //       )
-      //     : [...state.cartItems, { ...item, qty: 1 }],
-
-      //   totalQty: state.cartItems.reduce((acc, item) => acc + item.qty, 1),
-      // };
     }
 
     case REMOVE_FROM_CART: {
