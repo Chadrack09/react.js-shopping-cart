03cca3b94bc3520bca09e77cb24f3df776a89046
Chaadrack B <bbenolych@gmail.com>
2022-10-26 15:50:16 +0200

    created button styled-component config file

---
+++ src/components/styles/Button.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+export const ItemButton = styled.button`
+  background-color: ${props => props.bgColor};
+  width: ${props => props.width};
+  height: ${props => props.height};
+  border: ${props => props.border};
+  color: ${props => props.color};
+  font-size: ${props => props.fontSize};
+  font-weight: ${props => props.fontWeight ? props.fontWeight : "400"};
+  text-transform: uppercase;
+  cursor: pointer;
+`;
