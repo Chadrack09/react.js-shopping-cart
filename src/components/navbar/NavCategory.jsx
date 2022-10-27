9cf71d7de01dd6075edc3529f7654ef0198b8e6d
Chaadrack B <bbenolych@gmail.com>
2022-10-27 18:11:15 +0200

    updated navcategory component

--- src/components/navbar/NavCategory.jsx
+++ src/components/navbar/NavCategory.jsx
@@ -1,56 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setCategoryAction } from '../../redux/actions/FetchCategories';
 import { filterProductsByCategory } from '../../redux/actions/FetchProducts';
-import store from '../../redux/store';
-import { SET_CATEGORY } from '../../redux/types';
 
 class NavCategory extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      products: [],
-    }
-  }
-  
-  static getDerivedStateFromProps(props, state) {
-    // console.log('From getDerivedStateFromProps', props);
-    if(props.products.length > 0) {
-      return {
-        products: props.products,
-      }
-    }
-    return null;
+    this.categoryClickEvent = this.categoryClickEvent.bind(this);
   }
 
   componentDidCatch(error, info) {
     console.log(error.message, info);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    let category = this.props.categories
-      .filter(e => typeof e !== undefined).shift();
-    // console.log("From Did Update", prevProps);
-    
-    if (category !== undefined) {
-      let categorySelected = store.getState().categorySelected;
-      if(categorySelected.length === 0) {
-        store.dispatch({
-          type: SET_CATEGORY,
-          payload: category
-        });
-      }
-    }
-  }
-
   categoryClickEvent = (category) => () => {
-    store.dispatch({
-      type: SET_CATEGORY,
-      payload: category
-    });
-    this.props.filterProductsByCategory(
-        this.props.products, category.name);
+    this.props.setCategory(category);
+    this.props.filterProductsByCategory(this.props.products, category.name);
   }
 
   render() {
@@ -76,14 +43,14 @@ class NavCategory extends Component {
 const mapStateToProps = (state) => {
   return {
     products: state.products,
-    category: state.category,
     categories: state.categories,
   }
 }
 
-const  mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = dispatch => ({
   filterProductsByCategory: (products, category) => 
     dispatch(filterProductsByCategory(products, category)),
+  setCategory: (category) => dispatch(setCategoryAction(category))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavCategory);
